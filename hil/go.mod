ab5cbf7827537002268f5d79b515cc4211476409
Parham Alvani <parham.alvani@gmail.com>
2022-11-05 22:43:28 +0330

    feat: working with hil but go template is awesome too

---
+++ hil/go.mod
@@ -0,0 +1,9 @@
+module hil
+
+go 1.13
+
+require (
+	github.com/hashicorp/hil v0.0.0-20210521165536-27a72121fd40
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+)
---
+++ hil/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+
+	"github.com/hashicorp/hil"
+	"github.com/hashicorp/hil/ast"
+)
+
+func main() {
+	name := "main.rjn"
+
+	tmpl, err := ioutil.ReadFile(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tree, err := hil.Parse(string(tmpl))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	date := ast.Function{
+		ArgTypes:   []ast.Type{},
+		ReturnType: ast.TypeString,
+		Variadic:   false,
+		Callback: func(inputs []interface{}) (interface{}, error) {
+			return time.Now().Format("01-01-2006"), nil
+		},
+	}
+
+	for _, name := range []string{"P1", "P2", "P2"} {
+		config := &hil.EvalConfig{
+			GlobalScope: &ast.BasicScope{
+				VarMap: map[string]ast.Variable{
+					"name": {
+						Type:  ast.TypeString,
+						Value: name,
+					},
+				},
+				FuncMap: map[string]ast.Function{
+					"date": date,
+				},
+			},
+		}
+
+		result, err := hil.Eval(tree, config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s\n", result.Value)
+	}
+}
