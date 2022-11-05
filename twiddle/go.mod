937eefd54e2047a4abb450bd49e2f00b8c205593
Parham Alvani <parham.alvani@gmail.com>
2022-11-05 23:19:23 +0330

    feat: add an example for twiddle algorithm

---
+++ twiddle/go.mod
@@ -0,0 +1,3 @@
+module github.com/1995parham-learning/go101/twiddle
+
+go 1.19
---
+++ twiddle/twiddle_test.go
@@ -0,0 +1,40 @@
+package twiddle_test
+
+import (
+	"testing"
+
+	"github.com/1995parham-learning/go101/twiddle"
+)
+
+func TestBasic1(t *testing.T) {
+	tw := twiddle.New(2, 2)
+
+	b := tw.Next()
+	for _, v := range b {
+		if !v {
+			t.Fatalf("excpeting true found %v", v)
+		}
+	}
+}
+
+func TestBasic2(t *testing.T) {
+	tw := twiddle.New(1, 2)
+
+	table := []struct {
+		ex [2]bool
+	}{
+		{[2]bool{false, true}},
+		{[2]bool{true, false}},
+	}
+
+	i := 0
+
+	for b := tw.Next(); b != nil; b = tw.Next() {
+		for j := 0; j < len(b); j++ {
+			if table[i].ex[j] != b[j] {
+				t.Fatalf("excepting %v found %v", table[i].ex[j], b[j])
+			}
+		}
+		i++
+	}
+}
